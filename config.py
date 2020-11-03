8d3f89d97f197783905cb20eca2baca4e4ebf36e
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-03 07:07:02 +0300

    file structure

---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+
+
+class Config:
+     '''
+    General configuration parent class
+    '''
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    
+
+
+class ProdConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+
+
+
+
+class DevConfig(Config):
+        '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://bernard:86kerubo19@localhost/blog'
+
+    DEBUG = True
+
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
