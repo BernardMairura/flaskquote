a76ad819beddfad7fe03b2b771655f332d5b5868
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-03 07:36:19 +0300

    defining role class

--- app/models.py
+++ app/models.py
@@ -30,3 +30,15 @@ class User(db.Model,UserMixin):
         return check_password_hash(self.pass_secure,password)
 
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+    users = db.relationship('User',backref = 'role',lazy="dynamic")
+
+
+    def __repr__(self):
+        return f'User {self.name}'
+
+
