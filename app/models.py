d12500c3fc39ec8b48287cb265183f33a58d04ca
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-03 07:35:09 +0300

    Defining user class

--- app/models.py
+++ app/models.py
@@ -1,1 +1,32 @@
 from .import db
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from sqlalchemy import func
+from datetime import datetime
+
+
+class User(db.Model,UserMixin):
+    __tablename__ ='users'
+
+    id=db.Column(db.Integer,primary_key=True)
+    username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    pass_secure = db.Column(db.String(255))
+    blog = db.relationship('Blog', backref='user', lazy='dynamic')
+    comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+
+
+     @property
+    def password(self):
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.pass_secure,password)
+
+
---
+++ manage.py
@@ -0,0 +1,3 @@
+from app import config_options,db
+from flask_script import Manager,Server
+
