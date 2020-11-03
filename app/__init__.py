952b0385a4f538c0d5da053babe30bacf50426a2
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-03 07:23:50 +0300

    initializing flask for imporation

--- app/__init__.py
+++ app/__init__.py
@@ -2,3 +2,15 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
