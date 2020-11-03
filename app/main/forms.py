3e81a7a9eb1d07e0556e342b40a939755be44491
bernardmairura@gmail.com <bernardmairura@gmail.com>
2020-11-03 07:54:17 +0300

    create class for blogform and commentform

---
+++ app/main/forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm,Form
+from wtforms import StringField,TextAreaField,SubmitField,PasswordField,BooleanField,RadioField,ValidationError
+from wtforms.validators import required,email,equal_to
+
+
+
+class BlogForm(FlaskForm):
+	title = StringField('Title', validators=[Required()])
+	description = TextAreaField("Select your desired pitch?",validators=[Required()])
+	submit = SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+	description = TextAreaField('Add comment',validators=[Required()])
+	submit = SubmitField()
--- manage.py
+++ manage.py
@@ -1,3 +1,3 @@
 from app import config_options,db
 from flask_script import Manager,Server
-
+from app.models import User,Role,Comment,Blog
